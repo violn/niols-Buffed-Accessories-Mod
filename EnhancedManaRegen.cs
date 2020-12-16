e9e5459ada74b32c1bb95e5a82266440cfa65c56
niol <32370882+violn@users.noreply.github.com>
2020-12-16 16:54:45 -0700

    Add comments

--- EnhancedManaRegen.cs
+++ EnhancedManaRegen.cs
@@ -1,10 +1,12 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace niolsBuffedAccessories.Buffs
 {
+    //Handles things related to buffs added through mods
     public class EnhancedManaRegen : ModBuff
     {
+        //Set the default values for the buff
         public override void SetDefaults()
         {
             DisplayName.SetDefault("Enhanced Mana Regeneration");
@@ -14,6 +16,7 @@ namespace niolsBuffedAccessories.Buffs
             canBeCleared = false;
         }
 
+        //Set the effects of the buff
         public override void Update(Player player, ref int buffIndex)
         {
             player.manaRegenBonus += 80;
