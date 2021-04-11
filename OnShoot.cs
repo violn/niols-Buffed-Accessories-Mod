ba0e5c9ba6fde4d3678447a883991609220c7371
niol <32370882+violn@users.noreply.github.com>
2021-04-11 15:24:10 -0700

    Update OnShoot.cs

--- OnShoot.cs
+++ OnShoot.cs
@@ -1,32 +1,30 @@
 using Microsoft.Xna.Framework;
 using niolsBuffedAccessories;
-using References;
 using System;
 using Terraria;
 using Terraria.ModLoader;
 
-//Handles the things that happen when using a weapon that creates a projectile
 public class OnShoot : GlobalItem
 {
-    //Terraria hook that runs when shooting a projectile
+    public static Random rand = new Random();
+
     public override bool Shoot(Item item, Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
     {
-        Reference.itemUsed = item;
-        Random rand = new Random();
+        OnHitProj.itemUseTime = item.useTime;
 
-        //Duplicate a ranged item projectile if a scope accessory is equipped
-        if ((Reference.equippedRangerE || Reference.equippedSScope) && (item.ranged || Reference.blackListedItems.Contains(player.HeldItem.type)) && rand.Next(100) < 33)
+        //Duplicate a ranged item projectile if a scope accessory is equipped.
+        if ((AccessoryProperties.equippedRangerEmblem || AccessoryProperties.equippedSniperScope) && item.ranged && rand.Next(100) < 33)
         {
-            if (rand.Next(100) > CalcChance(item.useTime))
+            if (rand.Next(100) > OnHitProj.CalcChance(item.useTime))
             {
                 ProjectileHandler.CreateDuplicate(type, position, speedX, speedY, damage, knockBack);
             }
         }
 
-        //Create three yoyos when using a yoyo with a yoyobag equipped
-        if (item.melee && Reference.equippedYoyoBag && Reference.yoyoProj.Count > 0)
+        //Create three yoyos when using a yoyo with a yoyobag equipped.
+        if (item.melee && AccessoryProperties.equippedYoyoBag && ProjectileHandler.YoyoProj.Count > 0)
         {
-            foreach (Projectile proj in Reference.yoyoProj)
+            foreach (Projectile proj in ProjectileHandler.YoyoProj)
             {
                 if (proj.type == type)
                 {
@@ -39,53 +37,36 @@ public class OnShoot : GlobalItem
             }
         }
 
-        //Gets the players position to be used later for calculating damage based on distance
+        //Gets the players position to be used later for calculating damage based on distance.
         if (item.ranged)
         {
             for (int x = 0; x < 2; x++)
             {
-                Reference.playerPosition = new Vector2(player.position.X, player.position.Y);
+                OnHitProj.playerPosition = new Vector2(player.position.X, player.position.Y);
             }
         }
         return true;
     }
-
-    //Decides if a weapon should consume ammo when shot
+    
+    //Decides if a weapon should consume ammo when shot while having cetain ranger items equipped.
     public override bool ConsumeAmmo(Item item, Player player)
     {
-        Random rand = new Random();
         int chance = 0;
-        if (item.ranged && Reference.equippedRangerE)
+        if (item.ranged && AccessoryProperties.equippedRangerEmblem)
         {
             chance += 10;
         }
 
-        if (item.ranged && Reference.equippedSScope)
+        if (item.ranged && AccessoryProperties.equippedSniperScope)
         {
             chance += 15;
         }
 
-        if (item.ranged && Reference.equippedOWABuckler)
+        if (item.ranged && AccessoryProperties.equippedOOABuckler)
         {
             chance += 3;
         }
 
         return rand.Next(100) > chance;
-    }
-
-    //Caculates the chance of something happening based on the use time of the item
-    public static int CalcChance(int time)
-    {
-        if (time > 60)
-        {
-            return 60;
-        }
-
-        if (time < 11)
-        {
-            return 25;
-        }
-
-        return (int)(.25f + (.5f * 60f));
     }
 }
