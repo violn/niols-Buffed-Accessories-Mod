afb63e3608829a7d8e84442bee4e170551a20c40
niol <32370882+violn@users.noreply.github.com>
2021-04-11 15:20:11 -0700

    Update BuffPlayer.cs

--- BuffPlayer.cs
+++ BuffPlayer.cs
@@ -1,24 +1,78 @@
-﻿using References;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-//Handles the buffs given to the player
-public class BuffPlayer : GlobalNPC
+public class BuffPlayerHurt : GlobalNPC
 {
-    //Hook that handles actions when the player is hurt
     public override void OnHitPlayer(NPC npc, Player player, int damage, bool crit)
     {
-        //Adds the honey buff when a bee accessory is equipped
-        if (Reference.equippedBee)
+        //Gives the player the honeybuff on taking damage.
+        if (AccessoryProperties.equippedBee)
         {
             player.AddBuff(BuffID.Honey, 480, true);
         }
 
-        //Adds the enhanced mana regeneration buff when a star item is equipped
-        if (Reference.equippedStar)
+        //Gives the player the enhanced mana regen on taking damage.
+        if (AccessoryProperties.equippedStar)
         {
             player.AddBuff(ModContent.BuffType<niolsBuffedAccessories.Buffs.EnhancedManaRegen>(), 480, true);
+        }
+    }
+}
+
+public class BuffPlayerDamageProj: ModPlayer
+{
+    public override void ModifyHitNPCWithProj(Projectile proj, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+    {
+        //Gives Beserker Rage buff when killing an NPC with a melee projectile.
+        if (AccessoryProperties.equippedFireGauntlet)
+        {
+            BeserkerRage.beserkerRageStrength = 3;
+        }
+        else if (AccessoryProperties.equippedMechGlove)
+        {
+            BeserkerRage.beserkerRageStrength = 2;
+        }
+        else if (AccessoryProperties.equippedWarriorEmblem)
+        {
+            BeserkerRage.beserkerRageStrength = 1;
+        }
+
+        if ((AccessoryProperties.equippedMechGlove ||
+            AccessoryProperties.equippedFireGauntlet ||
+            AccessoryProperties.equippedWarriorEmblem) &&
+            proj.melee &&
+            target.life < damage)
+        {
+            player.AddBuff(ModContent.BuffType<BeserkerRage>(), 340, true);
+        }
+    }
+}
+
+public class BuffPlayerDamageMelee : GlobalItem
+{
+    //Gives Beserker Rage buff when killing an NPC with a melee attack.
+    public override void ModifyHitNPC(Item item, Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+    {
+        if (AccessoryProperties.equippedFireGauntlet)
+        {
+            BeserkerRage.beserkerRageStrength = 3;
+        }
+        else if (AccessoryProperties.equippedMechGlove)
+        {
+            BeserkerRage.beserkerRageStrength = 2;
+        }
+        else if (AccessoryProperties.equippedWarriorEmblem)
+        {
+            BeserkerRage.beserkerRageStrength = 1;
+        }
+
+        if ((AccessoryProperties.equippedMechGlove ||
+            AccessoryProperties.equippedFireGauntlet ||
+            AccessoryProperties.equippedWarriorEmblem) &&
+            target.life < damage)
+        {
+            player.AddBuff(ModContent.BuffType<BeserkerRage>(), 340, true);
         }
     }
 }
