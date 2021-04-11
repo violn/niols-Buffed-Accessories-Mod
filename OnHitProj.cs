8f0bfc51adad86bcf2d6b03eb92e48c6749146d7
niol <32370882+violn@users.noreply.github.com>
2021-04-11 15:23:47 -0700

    Update OnHitProj.cs

--- OnHitProj.cs
+++ OnHitProj.cs
@@ -1,71 +1,70 @@
 using Microsoft.Xna.Framework;
 using niolsBuffedAccessories;
-using References;
 using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-//Handles stuff when hitting a NPC with a projectile
 public class OnHitProj : GlobalProjectile
 {
     private static readonly Random rand = new Random();
+    public static Vector2 targetPosition;
+    public static Vector2 playerPosition;
+    public static int itemUseTime = 20;
+    public static int celestialRegen = 15;
+    public static double currentOnHitBoost = 0;
+    public static double depleteBoost = 0;
 
-    //Terraria hook that runs when hitting an NPC with projectile
     public override void ModifyHitNPC(Projectile proj, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
     {
-        //Increases ranged damage based on how far the player is from the NPC
-        if (proj.ranged && (Reference.equippedRScope || Reference.equippedSScope))
+        //Increases ranged damage based on how far the player is from the NPC.
+        if (proj.ranged && (AccessoryProperties.equippedRifleScope || AccessoryProperties.equippedSniperScope))
         {
-            Reference.targetPosition = new Vector2(target.position.X, target.position.Y);
+            targetPosition = new Vector2(target.position.X, target.position.Y);
             damage = CalcDamage(damage, proj);
         }
 
-        //Increase magic damage and mana regeneration when continously damaging a NPC
-        if (proj.magic && (Reference.equippedCeleE || Reference.equippedSorcE))
+        //Increase magic damage and mana regeneration when continously damaging a NPC.
+        if (proj.magic && (AccessoryProperties.equippedCelestialEmblem || AccessoryProperties.equippedSorcerorEmblem))
         {
-            Reference.delayCounter = 0;
-            Reference.counter = 0;
+            Counter.DelayCount = 0;
+            Counter.Count = 0;
             double stacksGained;
-            if (Reference.itemUsed != null)
-            {
-                stacksGained = CalcStacks(Reference.itemUsed.useTime);
-            }
-            else stacksGained = CalcStacks(25);
+            stacksGained = CalcStacks(itemUseTime);
 
-            Reference.currentOnHitBoost += stacksGained - Reference.depleteBoost;
-            Reference.depleteBoost = .0;
-            if (Reference.currentOnHitBoost < .0)
+            currentOnHitBoost += stacksGained - depleteBoost;
+            depleteBoost = .0;
+            if (currentOnHitBoost < .0)
             {
-                Reference.currentOnHitBoost = .0;
+                currentOnHitBoost = .0;
             }
 
-            if (Reference.currentOnHitBoost > .30)
+            if (currentOnHitBoost > .30)
             {
-                Reference.currentOnHitBoost = .30;
+                currentOnHitBoost = .30;
             }
 
-            if (Reference.currentOnHitBoost > .0 && Reference.currentOnHitBoost < .01)
+            if (currentOnHitBoost > .0 && currentOnHitBoost < .01)
             {
-                Reference.currentOnHitBoost = .01;
+                currentOnHitBoost = .01;
             }
 
-            if (Reference.equippedCeleE)
+            if (AccessoryProperties.equippedCelestialEmblem)
             {
-                damage = (int)(damage * (1 + Reference.currentOnHitBoost));
-              
-                if(Reference.celeRegen < 60)
+                damage = (int)(damage * (1 + currentOnHitBoost));
+
+                if (celestialRegen < 60)
                 {
-                    Reference.celeRegen = (int)(Reference.celeRegen + (2 * (Reference.currentOnHitBoost * 100)));
+                    celestialRegen = (int)(celestialRegen + (2 * (currentOnHitBoost * 100)));
                 }
             }
             else
             {
-                damage = (int)(damage * (1 + (Reference.currentOnHitBoost / 2)));
+                damage = (int)(damage * (1 + (currentOnHitBoost / 2)));
             }
         }
-
-        //Chance to spawn plague bees on hit
+        
+        //Chance to spawn plague bees on hit.
         if (proj.type != ProjectileID.HallowStar &&
             proj.type != ProjectileID.GiantBee &&
             proj.type != ProjectileID.Bee &&
@@ -74,35 +73,20 @@ public class OnHitProj : GlobalProjectile
             proj.type != ProjectileID.SporeGas3 &&
             proj.type != ProjectileID.SporeTrap &&
             proj.type != ProjectileID.SporeTrap2 &&
-            Reference.cal != null && proj.type != Reference.cal.ProjectileType("PlaguenadeBee") &&
-            Reference.cal != null && proj.type != Reference.cal.ProjectileType("PlagueSeeker"))
+            BuffedAccessories.calamity != null && proj.type != BuffedAccessories.calamity.ProjectileType("PlaguenadeBee") &&
+            BuffedAccessories.calamity != null && proj.type != BuffedAccessories.calamity.ProjectileType("PlagueSeeker"))
         {
-            if (Reference.itemUsed != null)
+            if (AccessoryProperties.equippedPlagueHive)
             {
-                if (Reference.equippedPlague)
+                if (rand.Next(100) < CalcChance(itemUseTime))
                 {
-                    if (rand.Next(100) < CalcChance(Reference.itemUsed.useTime))
-                    {
-                        ProjectileHandler.CreatePlagueBees(target, damage);
-                        Reference.equippedBee = false;
-                    }
-                }
-            }
-
-            else
-            {
-                if (Reference.equippedPlague)
-                {
-                    if (rand.Next(100) < CalcChance(20))
-                    {
-                        ProjectileHandler.CreatePlagueBees(target, damage);
-                        Reference.equippedBee = false;
-                    }
+                    ProjectileHandler.CreatePlagueBees(target, damage);
+                    AccessoryProperties.equippedBee = false;
                 }
             }
         }
 
-        //Chance to spawn bees and star on hit
+        //Chance to spawn bees and star on hit.
         if (proj.type != ProjectileID.HallowStar &&
             proj.type != ProjectileID.GiantBee &&
             proj.type != ProjectileID.Bee &&
@@ -111,96 +95,106 @@ public class OnHitProj : GlobalProjectile
             proj.type != ProjectileID.SporeGas3 &&
             proj.type != ProjectileID.SporeTrap &&
             proj.type != ProjectileID.SporeTrap2 &&
-            Reference.cal != null && proj.type != Reference.cal.ProjectileType("PlaguenadeBee") &&
-            Reference.cal != null && proj.type != Reference.cal.ProjectileType("PlagueSeeker"))
+            BuffedAccessories.calamity == null)
         {
-            if (Reference.itemUsed != null)
+            if (AccessoryProperties.equippedBee && AccessoryProperties.equippedStar)
             {
-                if (Reference.equippedBee && Reference.equippedStar)
+                if (rand.Next(100) < CalcChance(itemUseTime))
                 {
-                    if (rand.Next(100) < CalcChance(Reference.itemUsed.useTime))
-                    {
-                        ProjectileHandler.CreateStars(target, damage);
-                    }
+                    ProjectileHandler.CreateStars(target, damage);
+                }
 
-                    if (rand.Next(100) < CalcChance(Reference.itemUsed.useTime))
-                    {
-                        if (Reference.equippedHive && rand.Next(2) == 1)
-                        {
-                            ProjectileHandler.CreateStrongBees(target, damage);
-                        }
-                        else ProjectileHandler.CreateWeakBees(target, damage);
-                    }
-                }
-                else if (Reference.equippedBee && !Reference.equippedStar)
+                if (rand.Next(100) < CalcChance(itemUseTime))
                 {
-                    if (rand.Next(100) < CalcChance(Reference.itemUsed.useTime))
+                    if (AccessoryProperties.equippedHive && rand.Next(2) == 1)
                     {
-                        if (Reference.equippedHive && rand.Next(2) == 1)
-                        {
-                            ProjectileHandler.CreateStrongBees(target, damage);
-                        }
-                        else ProjectileHandler.CreateWeakBees(target, damage);
+                        ProjectileHandler.CreateStrongBees(target, damage);
                     }
-                }
-                else if (!Reference.equippedBee && Reference.equippedStar)
-                {
-                    if (rand.Next(100) < CalcChance(Reference.itemUsed.useTime))
-                    {
-                        ProjectileHandler.CreateStars(target, damage);
-                    }
+                    else ProjectileHandler.CreateWeakBees(target, damage);
                 }
             }
-            else
+            else if (AccessoryProperties.equippedBee && !AccessoryProperties.equippedStar)
             {
-                if (Reference.equippedBee && Reference.equippedStar)
+                if (rand.Next(100) < CalcChance(itemUseTime))
                 {
-                    if (rand.Next(100) < CalcChance(20))
+                    if (AccessoryProperties.equippedHive && rand.Next(2) == 1)
                     {
-                        ProjectileHandler.CreateStars(target, damage);
+                        ProjectileHandler.CreateStrongBees(target, damage);
                     }
+                    else ProjectileHandler.CreateWeakBees(target, damage);
+                }
+            }
+            else if (!AccessoryProperties.equippedBee && AccessoryProperties.equippedStar)
+            {
+                if (rand.Next(100) < CalcChance(itemUseTime))
+                {
+                    ProjectileHandler.CreateStars(target, damage);
+                }
+            }
+        }
 
-                    if (rand.Next(100) < CalcChance(20))
-                    {
-                        if (Reference.equippedHive && rand.Next(2) == 1)
-                        {
-                            ProjectileHandler.CreateStrongBees(target, damage);
-                        }
-                        else ProjectileHandler.CreateWeakBees(target, damage);
-                    }
-                }
-                else if (Reference.equippedBee && !Reference.equippedStar)
+        //Chance to spawn bees and star on hit if calamity is enabled.
+        else if (proj.type != ProjectileID.HallowStar &&
+            proj.type != ProjectileID.GiantBee &&
+            proj.type != ProjectileID.Bee &&
+            proj.type != ProjectileID.SporeGas &&
+            proj.type != ProjectileID.SporeGas2 &&
+            proj.type != ProjectileID.SporeGas3 &&
+            proj.type != ProjectileID.SporeTrap &&
+            proj.type != ProjectileID.SporeTrap2 &&
+            BuffedAccessories.calamity != null && proj.type != BuffedAccessories.calamity.ProjectileType("PlaguenadeBee") &&
+            BuffedAccessories.calamity != null && proj.type != BuffedAccessories.calamity.ProjectileType("PlagueSeeker"))
+        if (AccessoryProperties.equippedBee && AccessoryProperties.equippedStar)
+        {
+            if (rand.Next(100) < CalcChance(itemUseTime))
+            {
+                    ProjectileHandler.CreateStars(target, damage);
+            }
+
+            if (rand.Next(100) < CalcChance(itemUseTime))
+            {
+                if (AccessoryProperties.equippedHive && rand.Next(2) == 1)
                 {
-                    if (rand.Next(100) < CalcChance(20))
-                    {
-                        if (Reference.equippedHive && rand.Next(2) == 1)
-                        {
-                            ProjectileHandler.CreateStrongBees(target, damage);
-                        }
-                        else ProjectileHandler.CreateWeakBees(target, damage);
-                    }
+                        ProjectileHandler.CreateStrongBees(target, damage);
                 }
-                else if (!Reference.equippedBee && Reference.equippedStar)
+                else ProjectileHandler.CreateWeakBees(target, damage);
+            }
+        }
+        else if (AccessoryProperties.equippedBee && !AccessoryProperties.equippedStar)
+        {
+            if (rand.Next(100) < CalcChance(itemUseTime))
+            {
+                if (AccessoryProperties.equippedHive && rand.Next(2) == 1)
                 {
-                    if (rand.Next(100) < CalcChance(20))
-                    {
-                        ProjectileHandler.CreateStars(target, damage);
-                    }
+                        ProjectileHandler.CreateStrongBees(target, damage);
                 }
+                else ProjectileHandler.CreateWeakBees(target, damage);
+            }
+        }
+        else if (!AccessoryProperties.equippedBee && AccessoryProperties.equippedStar)
+        {
+            if (rand.Next(100) < CalcChance(itemUseTime))
+            {
+                    ProjectileHandler.CreateStars(target, damage);
             }
         }
     }
 
-    //Calculates damage based on distance the player is from the NPC
+    /// <summary>
+    /// Calculates damage based on distance the player is from the NPC.
+    /// </summary>
+    /// <param name="damage">The original damage.</param>
+    /// <param name="proj">The projectile that did that damage.</param>
+    /// <returns>Boosted damage.</returns>
     public static int CalcDamage(int damage, Projectile proj)
     {
         double distance;
         double velocityBooster;
         double damageMulti;
         double calcMulti;
-        if (Reference.equippedSScope)
+        if (AccessoryProperties.equippedSniperScope)
         {
-            distance = CalcDistance(Reference.playerPosition, Reference.targetPosition);
+            distance = CalcDistance(playerPosition, targetPosition);
             velocityBooster = Math.Abs(proj.velocity.X) + Math.Abs(proj.velocity.Y);
             if (velocityBooster < 1)
             {
@@ -220,9 +214,9 @@ public class OnHitProj : GlobalProjectile
             }
             return (int)(damage * calcMulti);
         }
-        else if (Reference.equippedRScope)
+        else if (AccessoryProperties.equippedRifleScope)
         {
-            distance = CalcDistance(Reference.playerPosition, Reference.targetPosition);
+            distance = CalcDistance(playerPosition, targetPosition);
             velocityBooster = Math.Abs(proj.velocity.X) + Math.Abs(proj.velocity.Y);
             if (velocityBooster < 1)
             {
@@ -246,20 +240,33 @@ public class OnHitProj : GlobalProjectile
         return damage;
     }
 
-    //Calculates distance using the distance formula
+    /// <summary>
+    /// Calculates distance using the distance formula.
+    /// </summary>
+    /// <param name="player">The player's position.</param>
+    /// <param name="target">The target's position.</param>
+    /// <returns>Their distance from eachother.</returns>
     public static float CalcDistance(Vector2 player, Vector2 target)
     {
         return (float)Math.Sqrt(Math.Pow((target.X - player.X), 2) + Math.Pow((target.Y - player.Y), 2));
     }
 
-    //Calculates stacks gained depending on the the item use time
+    /// <summary>
+    /// Calculates stacks gained depending on the the item use time.
+    /// </summary>
+    /// <param name="use">The items use time.</param>
+    /// <returns>The amount of stacks the player gets.</returns>
     public static double CalcStacks(int use)
     {
         double useBoost = (double)(use * ((277m / 60m) / 1500m));
         return Math.Round(useBoost, 2, MidpointRounding.AwayFromZero);
     }
 
-    //Calculates the chance to spawn a projectile based on item use time
+    /// <summary>
+    /// Calculates the chance for something to happen based on used time.
+    /// </summary>
+    /// <param name="time">The use time of the item.</param>
+    /// <returns>The chance of something happening.</returns>
     public static int CalcChance(int time)
     {
         if (time > 60)
@@ -273,36 +280,5 @@ public class OnHitProj : GlobalProjectile
         }
 
         return (int)(30f + (.8f * 60f));
-    }
-}
-
-//Handles when the player hits a NPC with a melee projectile
-public class OnHitProjMelee : ModPlayer
-{
-    //Tmodloader hook that runs when hitting an NPC
-    public override void ModifyHitNPCWithProj(Projectile proj, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
-    {
-        //Gives Beserker Rage buff when killing an NPC with a melee projectile
-        if (Reference.equippedFGaunt)
-        {
-            Reference.buffCheck1 = 1;
-        }
-        else if (Reference.equippedMechGlove)
-        {
-            Reference.buffCheck2 = 1;
-        }
-        else if (Reference.equippedWarE)
-        {
-            Reference.buffCheck3 = 1;
-        }
-
-        if ((Reference.equippedMechGlove ||
-            Reference.equippedFGaunt ||
-            Reference.equippedWarE) &&
-            proj.melee &&
-            target.life < damage)
-        {
-            player.AddBuff(ModContent.BuffType<niolsBuffedAccessories.Buffs.BeserkerRage>(), 340, true);
-        }
     }
 }
