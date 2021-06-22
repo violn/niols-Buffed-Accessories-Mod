653956c80d1c89c1d23530142cdd4c92ec27fa45
niol <32370882+violn@users.noreply.github.com>
2021-06-22 15:22:28 -0700

    Update BeserkerRage.cs

--- BeserkerRage.cs
+++ BeserkerRage.cs
@@ -5,13 +5,6 @@ namespace niolsBuffedAccessories.Buffs
 {
     public class BeserkerRage : ModBuff
     {
-        /// <summary>
-        /// Determines the strength of the beserker rage buff.
-        /// Goes from 1 to 3 with 1 being the weakest and 3 being the strongest.
-        /// The strength is determined by the cetain melee accessories.
-        /// </summary>
-        public static int beserkerRageStrength = 0;
-
         public override void SetDefaults()
         {
             DisplayName.SetDefault("Beserker Rage");
@@ -23,54 +16,16 @@ namespace niolsBuffedAccessories.Buffs
 
         public override void Update(Player player, ref int buffIndex)
         {
-            switch (beserkerRageStrength)
-            {
-                case 1:
-                    player.meleeCrit += 75;
-                    player.meleeDamage += .30f;
-                    break;
-
-                case 2:
-                    player.meleeCrit += 50;
-                    player.meleeDamage += .15f;
-                    break;
-
-                case 3:
-                    player.meleeCrit += 25;
-                    player.meleeDamage += .8f;
-                    break;
-            }
+            player.meleeCrit += 30;
+            player.meleeDamage += .15f;
         }
 
         public override bool ReApply(Player player, int time, int buffIndex)
         {
-            switch (beserkerRageStrength)
-            {
-                case 1:
-                    player.meleeCrit -= 75;
-                    player.meleeDamage -= .30f;
-
-                    player.meleeCrit += 75;
-                    player.meleeDamage += .30f;
-                    break;
-
-                case 2:
-                    player.meleeCrit -= 50;
-                    player.meleeDamage -= .15f;
-
-                    player.meleeCrit += 50;
-                    player.meleeDamage += .15f;
-                    break;
-
-                case 3:
-                    player.meleeCrit -= 25;
-                    player.meleeDamage -= .8f;
-
-                    player.meleeCrit += 25;
-                    player.meleeDamage += .8f;
-                    break;
-            }
-
+            player.meleeCrit -= 30;
+            player.meleeDamage -= .15f;
+            player.meleeCrit += 30;
+            player.meleeDamage += .15f;
             return false;
         }
     }
