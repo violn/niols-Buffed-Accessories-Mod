46fc40eb8553abfe9762046ec8f8f05f9727c3a6
niol <32370882+violn@users.noreply.github.com>
2021-06-22 15:35:38 -0700

    Update and rename PreventDeath.cs to CrossNecklace.cs

--- CrossNecklace.cs
+++ CrossNecklace.cs
@@ -1,19 +1,15 @@
-﻿using System;
+using niolsBuffedAccessories;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
 
-//Gives a chance of death prevention when the cross necklace is equipped.
-public class PreventDeath : ModPlayer
+public class CrossNecklace : ModPlayer
 {
-    public static Random ran = new Random();
-
     public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
     {
-        if (damage > player.statLife && AccessoryProperties.EquippedCrossNecklace && ran.Next(100) < 10)
+        if (AccessoryProperties.equippedCrossNecklace && BuffedAccessories.ran.Next(100) < 6)
         {
             damage = player.statLife - 1;
         }
-
         return base.PreHurt(pvp, quiet, ref damage, ref hitDirection, ref crit, ref customDamage, ref playSound, ref genGore, ref damageSource);
     }
 }
