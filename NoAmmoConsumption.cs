391b97b7cfdae700ea3ea84e7fac410d9d1a0f8f
niol <32370882+violn@users.noreply.github.com>
2021-06-24 11:16:42 -0700

    Update NoAmmoConsumption.cs

--- NoAmmoConsumption.cs
+++ NoAmmoConsumption.cs
@@ -1,4 +1,4 @@
-﻿using niolsBuffedAccessories;
+using niolsBuffedAccessories;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -7,12 +7,10 @@ public class NoAmmoConsumption : GlobalItem
     public override bool ConsumeAmmo(Item item, Player player)
     {
         int chance = 0;
-        if (item.ranged)
-        {
-            chance += AccessoryProperties.equippedRangerEmblem ? 10 : 0;
-            chance += AccessoryProperties.equippedSniperScope ? 15 : 0;
-            chance += AccessoryProperties.equippedOOABuckler ? 2 : 0;
-        }
-        return BuffedAccessories.ran.Next(1, 100) < chance;
+        chance += AccessoryProperties.equippedRangerEmblem ? 10 : 0;
+        chance += AccessoryProperties.equippedSniperScope ? 15 : 0;
+        chance += AccessoryProperties.equippedOOABuckler ? 2 : 0;
+
+        return BuffedAccessories.ran.Next(100) > chance;
     }
 }
