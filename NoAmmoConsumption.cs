55249d7df1c73ff66277ad4337066e1fd0f45f19
violn <donedisontumaini@gmail.com>
2021-09-20 17:29:53 -0700

    Code changes for 1.2.1

--- NoAmmoConsumption.cs
+++ NoAmmoConsumption.cs
@@ -5,7 +5,7 @@ namespace niolsBuffedAccessories.Buffed
 {
     public class NoAmmoConsumption : GlobalItem
     {
-        public override bool ConsumeAmmo(Item item, Player player)
+        public override bool CanConsumeAmmo(Item item, Player player)
         {
             int chance = 0;
             chance += AccessoryProperties.equippedRangerEmblem ? 10 : 0;
--- Pygmy.cs
+++ Pygmy.cs
@@ -1,17 +1,27 @@
-﻿using Terraria;
+﻿using System.Collections.Generic;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace niolsBuffedAccessories.Buffed
 {
     public class Pygmy : GlobalProjectile
     {
+        public static List<Projectile> modifiedPorjectiles = new();
+
         public override void OnHitNPC(Projectile proj, NPC target, int damage, float knockback, bool crit)
         {
-            if ((proj.sentry || proj.minion) && AccessoryProperties.equippedPygmyNecklace)
+            if ((proj.sentry || proj.minion) && AccessoryProperties.equippedPygmyNecklace && !proj.usesLocalNPCImmunity)
             {
                 proj.usesLocalNPCImmunity = true;
-                proj.localNPCImmunity[target.whoAmI] = 10;
+                proj.localNPCImmunity[target.whoAmI] = 2;
                 target.immune[proj.owner] = 0;
+                modifiedPorjectiles.Add(proj);
+            }
+
+            else if (modifiedPorjectiles.Contains(proj))
+            {
+                proj.usesLocalNPCImmunity = false;
+                modifiedPorjectiles.Remove(proj);
             }
         }
     }
